a44ee03164a76cc98d0f1f3d820b1dc05e42deec
SudhaMacherla <77689475+SudhaMacherla@users.noreply.github.com>
2021-02-21 19:26:09 -0500

    These are new commits
    These are new database created and committed.

---
+++ index.php
@@ -0,0 +1,28 @@
+<?php
+
+require_once("class.main.php");
+$o = new main;
+
+#print_r($_SERVER['REQUEST_URI']);
+
+
+if(isset($_SERVER['REQUEST_URI'])){
+    $array = explode('/', $_SERVER['REQUEST_URI']);
+    if(isset($array[0])){ $x = $array[0]; }
+    if(isset($array[1])){ $o->api = $array[1]; }
+    if(isset($array[2])){ $o->std_id = $array[2]; }
+    if(isset($array[3])){ $o->action = $array[3]; }
+    
+    
+    if($o->api != 'api' || !$o->students_api) {
+        $arr = array("error" => "Wrong Query.");
+        print_r(JSON_ENCODE($arr));        
+    }
+
+    print_r($o->takecall());
+}
+else{
+    $arr = array("error" => "Something went wrong.");
+    print_r(JSON_ENCODE($arr));
+}
+?>
